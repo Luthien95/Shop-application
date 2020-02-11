c1df5ce6833c355fd9251aee8abf85b2d57e1500
Aleksandra Salak <aleksandrasalak@gmail.com>
2020-02-12 00:17:21 +0100

    Init project

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyArNFVXLAqOTzV9dh3QHlihnBfHDOsIJB8",
+    authDomain: "shop-application-d2eeb.firebaseapp.com",
+    databaseURL: "https://shop-application-d2eeb.firebaseio.com",
+    projectId: "shop-application-d2eeb",
+    storageBucket: "shop-application-d2eeb.appspot.com",
+    messagingSenderId: "1063451033244",
+    appId: "1:1063451033244:web:16c0fb48c55677f90ff759",
+    measurementId: "G-H03MN7MPG8"
+  }
 };
