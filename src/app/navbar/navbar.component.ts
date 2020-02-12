f0f644d5533d2d5537ba2aeaa940988c1dd6fa6d
Aleksandra Salak <aleksandrasalak@gmail.com>
2020-02-13 00:33:14 +0100

    Add authentication

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { AuthService } from "../auth.service";
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.sass']
+  selector: "app-navbar",
+  templateUrl: "./navbar.component.html",
+  styleUrls: ["./navbar.component.sass"]
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent {
+  constructor(public auth: AuthService) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  logout() {
+    this.auth.logout();
   }
-
 }
