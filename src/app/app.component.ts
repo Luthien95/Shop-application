d0750c2f58eedf6c15dfb6e8b004188cd0040cc1
Aleksandra Salak <aleksandrasalak@gmail.com>
2020-02-13 23:12:37 +0100

    Authentication

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { AuthService } from "./auth.service";
+import { Component } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.sass"]
 })
 export class AppComponent {
-  title = 'shop-application';
+  constructor(private auth: AuthService, router: Router) {
+    auth.user$.subscribe(user => {
+      if (user) {
+        let returnUrl = localStorage.getItem("returnUrl");
+        router.navigateByUrl(returnUrl);
+      }
+    });
+  }
 }
--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from "@angular/core";
-import { Observable } from "rxjs/Observable";
 import { AngularFireAuth } from "angularfire2/auth";
 import * as firebase from "firebase";
+import { Observable } from "rxjs";
+import { ActivatedRoute } from "@angular/router";
 
 @Injectable({
   providedIn: "root"
@@ -9,11 +10,14 @@ import * as firebase from "firebase";
 export class AuthService {
   user$: Observable<firebase.User>;
 
-  constructor(private afAuth: AngularFireAuth) {
+  constructor(private afAuth: AngularFireAuth, private route: ActivatedRoute) {
     this.user$ = afAuth.authState;
   }
 
   login() {
+    let returnUrl = this.route.snapshot.queryParamMap.get("returnUrl") || "/";
+    localStorage.setItem("returnUrl", returnUrl);
+
     this.afAuth.auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider());
   }
 
